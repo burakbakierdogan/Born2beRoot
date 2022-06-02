6500a019ca62fe8a0dd80d8b4dc1daedb41191e1
burak baki erdogan <bbe2007@outlook.com>
2022-06-02 11:36:19 -0400

    sudo update

--- monitoring.sh
+++ monitoring.sh
@@ -1,10 +1,10 @@
-#! /bin/bash
+#!/bin/sh
 
 wall "	#Architecture:$(uname -a)
 	#CPU physical : $(nproc)
 	#vCPU : $(cat /proc/cpuinfo | grep processor | wc -l)
 	#Memory Usage: $(free -m | grep Mem: | awk '{print $3,"/",$2, "(",100*$3/$2, "%",")"}')
-	#Disk Usage: $(df | awk 'NR>1 {total += $3} END {print total/1024,"/ 27Gb","(",(100*total/1024)/(1024*27),"%",")"}')
+	#Disk Usage: $(df | awk 'NR>1 {total += $3} END {print total/1024,"/ 28Gb","(",(100*total/1024)/(1024*28),"%",")"}')
 	#CPU load: $(mpstat | grep all | awk '{print 100-$13,"%"}')
 	#Last boot: $(who -b | awk '{print $3, $4}')
 	#LVM use: $(if grep -q mapper /etc/fstab
@@ -15,7 +15,7 @@ wall "	#Architecture:$(uname -a)
 			fi)
 	#Connections TCP : $(ss -s | grep TCP: | awk '{print $4}' | sed 's/,/ /') ESTABLISHED
 	#User Log: $(w | awk ' NR>2 {print $0}' | wc -l)
-	#Network: IP $(ifconfig -a | grep -m 1 inet | awk '{print $2}') ($(ip addr show | grep link/ether | awk '{print $2}'))
-	#Sudo : $( grep --text sudo: /var/log/auth.log | wc -l) cmd
+	#Network: IP $(hostname -I) ($(ip addr show | grep link/ether | awk '{print $2}'))
+	#Sudo : $( cat /var/log/sudo/sudo.log | grep "COMMAND=" | wc -l) cmd
 	"
 
